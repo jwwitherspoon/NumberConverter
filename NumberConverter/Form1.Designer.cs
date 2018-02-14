e12caf83ddcc31aa5a670c011283c50ef4ad39e7
Joel Witherspoon <jwwitherspoon@gmail.com>
2018-02-14 14:18:47 -0500

    First full build.

---
+++ NumberConverter/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace NumberConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnSwap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(75, 93);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(232, 26);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(383, 93);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(232, 26);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Decimal",
+            "Hexadecimal",
+            "Binary"});
+            this.comboBox1.Location = new System.Drawing.Point(75, 167);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(232, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Decimal",
+            "Hexadecimal",
+            "Binary"});
+            this.comboBox2.Location = new System.Drawing.Point(383, 167);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(232, 28);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "to";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(383, 250);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(113, 47);
+            this.btnConvert.TabIndex = 5;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnSwap
+            // 
+            this.btnSwap.Location = new System.Drawing.Point(194, 250);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(113, 47);
+            this.btnSwap.TabIndex = 6;
+            this.btnSwap.Text = "Swap";
+            this.btnSwap.UseVisualStyleBackColor = true;
+            this.btnSwap.Click += new System.EventHandler(this.btnSwap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 361);
+            this.Controls.Add(this.btnSwap);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Number Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnSwap;
+    }
+}
+
---
+++ NumberConverter/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 1;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //Get the currently selected index from each combo box
+            int index1 = comboBox1.SelectedIndex;
+            int index2 = comboBox2.SelectedIndex;
+
+            //Decimal to hexadecimal
+            if (index1 == 0 && index2 == 1)
+            {
+                string input = "";
+                if (txtInput.Text != "")
+                    input = txtInput.Text;
+                string output = Convert.ToString(Convert.ToInt32(input, 10), 16);
+                txtOutput.Text = output;
+            }
+
+            //Decimal to binary
+            if (index1 == 0 && index2 == 2)
+            {
+                string input = "";
+                if (txtInput.Text != "")
+                    input = txtInput.Text;
+                string output = Convert.ToString(Convert.ToInt32(input, 10), 2);
+                txtOutput.Text = output;
+            }
+
+            //Hexadecimal to decimal
+            if (index1 == 1 && index2 == 0)
+            {
+                string input = "";
+                if (txtInput.Text != "")
+                    input = txtInput.Text;
+                string output = Convert.ToString(Convert.ToInt32(input, 16), 10);
+                txtOutput.Text = output;
+            }
+
+            //Hexadecimal to binary
+            if (index1 == 1 && index2 == 2)
+            {
+                string input = "";
+                if (txtInput.Text != "")
+                    input = txtInput.Text;
+                string output = Convert.ToString(Convert.ToInt32(input, 16), 2);
+                txtOutput.Text = output;
+            }
+
+            //Binary to decimal
+            if (index1 == 2 && index2 == 0)
+            {
+                string input = "";
+                if (txtInput.Text != "")
+                    input = txtInput.Text;
+                string output = Convert.ToString(Convert.ToInt32(input, 2), 10);
+                txtOutput.Text = output;
+            }
+
+            //Binary to hexadecimal
+            if (index1 == 2 && index2 == 1)
+            {
+                string input = "";
+                if (txtInput.Text != "")
+                    input = txtInput.Text;
+                string output = Convert.ToString(Convert.ToInt32(input, 2), 16);
+                txtOutput.Text = output;
+            }
+
+            //Redundant self-conversions
+            if (index1 == index2)
+                txtOutput.Text = txtInput.Text;
+        }
+
+        private void btnSwap_Click(object sender, EventArgs e)
+        {
+            //Swap the selected indexes of the combo boxes
+            int i = comboBox1.SelectedIndex;
+            comboBox1.SelectedIndex = comboBox2.SelectedIndex;
+            comboBox2.SelectedIndex = i;
+
+            //Swap the text in the text boxes
+            string s = txtInput.Text;
+            txtInput.Text = txtOutput.Text;
+            txtOutput.Text = s;
+        }
+    }
+}
